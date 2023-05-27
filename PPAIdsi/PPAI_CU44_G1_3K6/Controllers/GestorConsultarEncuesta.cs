db75418e1544f8067d7452fc1bc1f83fc41c35c9
LucaBessone <bessonelucasebastian@gmail.com>
2023-05-27 13:55:16 -0300

    Creación Back

---
+++ PPAIdsi/PPAI_CU44_G1_3K6/Controllers/GestorConsultarEncuesta.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PPAI_CU44_G1_3K6.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GestorConsultarEncuesta : Controller
+    {
+        [HttpGet]
+        public bool tomarOpcionCE()
+        {
+            return true;
+        }
+
+        [HttpGet]
+        [Route("api/tomarPeriodo")]
+        public List<string> tomarPeriodo()
+        {
+            var lista = new List<string>();
+            lista.Add("llamada1");
+            lista.Add("llamada2");
+            lista.Add("llamada3");
+            return lista;
+        }
+    }
+}
---
+++ PPAIdsi/PPAI_CU44_G1_3K6/Entidades/Encuesta.cs
@@ -0,0 +1,6 @@
+﻿namespace PPAI_CU44_G1_3K6.Entidades
+{
+    public class Encuesta
+    {
+    }
+}
