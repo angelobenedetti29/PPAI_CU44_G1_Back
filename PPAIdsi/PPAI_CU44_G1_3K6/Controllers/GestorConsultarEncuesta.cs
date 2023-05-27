79587907426695803d59428e52b8fc0c4d9b850f
LucaBessone <bessonelucasebastian@gmail.com>
2023-05-27 14:24:52 -0300

    Metodos creados 1

--- PPAIdsi/PPAI_CU44_G1_3K6/Controllers/GestorConsultarEncuesta.cs
+++ PPAIdsi/PPAI_CU44_G1_3K6/Controllers/GestorConsultarEncuesta.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PPAI_CU44_G1_3K6.Entidades;
 
 namespace PPAI_CU44_G1_3K6.Controllers
 {
@@ -8,6 +9,7 @@ namespace PPAI_CU44_G1_3K6.Controllers
     public class GestorConsultarEncuesta : Controller
     {
         [HttpGet]
+        [Route("api/tomarOpcionCE")]
         public bool tomarOpcionCE()
         {
             return true;
@@ -15,13 +17,37 @@ namespace PPAI_CU44_G1_3K6.Controllers
 
         [HttpGet]
         [Route("api/tomarPeriodo")]
-        public List<string> tomarPeriodo()
+        //devuelve lista de objetos llamada
+        public List<string> tomarPeriodo(DateTime fechaInicio, DateTime fechaFin)
         {
             var lista = new List<string>();
             lista.Add("llamada1");
             lista.Add("llamada2");
             lista.Add("llamada3");
             return lista;
+        }
+
+        [HttpGet]
+        [Route("api/tomarSeleccionLlamada")]
+        //crear objeto llamadaseleccionada
+        //string nombre, string estado, string duracion, string descripcionEncuesta, lista de objetos de abajo
+
+        //creo objeto PreguntaRespuesta y paso lista de esos
+        //atributos:
+        //string pregunta, string respuesta
+        public void tomarSeleccionLlamada(Llamada seleccionadoLlamada)
+        {
+
+        }
+
+
+        [HttpGet]
+        [Route("api/tomarOpcionDeImpresion")]
+        //recibo objeto llamadaSeleccionada
+        //devuelvo el csv creado
+        public string tomarOpcionDeImpresion()
+        {
+            return "devolucion";
         }
     }
 }
